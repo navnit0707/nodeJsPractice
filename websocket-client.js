76d85b0a284eebd85c57077a3869365fd6fd3b06
navnit0707 <knavnit81@gmail.com>
2022-03-12 13:14:24 +0530

    server connection and inner html updates

--- websocket-client.js
+++ websocket-client.js
@@ -4,6 +4,18 @@ WS.onmessage=(payload)=>{
     console.log(payload.data);
 };
 
+WS.onopen = ()=>{
+    displayTitle('connected to server');
+    console.log('Connection is open');
+};
+
+function displayTitle(title){
+    document.querySelector('h1').innerHTML = title;
+}
+
+WS.onclose =()=>{
+    console.log('connection closed');
+}
 document.forms[0].onsubmit = ()=>{
    let input =  document.getElementById('message');
     console.log(input.value);
